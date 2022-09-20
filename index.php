25f2c45162d2d17f21653a5ac3301fc5078ba9b1
Smurrowz <nigeldn1995@gmail.com>
2022-09-20 13:23:40 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$p = 'Lorem, ipsum dolor sit amet consectetur  sit adipisicing elit. At officia odit qui hic quibusdam dicta, sit adipisci repellendus temporibus odio corrupti sit corporis facere, nihil recusandae ad, blanditiis tempore nobis itaque neque.';
+$l = strlen($p);
+
+$w = $_GET['word'];
+$r = $_GET['replace'];
+$cussWords = str_replace($w, $r, $p, $count);
+
+echo $p;
+echo '<br>';
+echo $l;
+echo '<br>';
+
+
+?>
+
+<h3>Paragrafo originale: <?php echo $p?></h3>
+<p>la lunghezza del paragrafo e' <?php echo $l ?></p>
+<h3> Paragrafo modificato :<?php echo $cussWords ?></h3>
+<p> la parola era presente <?php echo $count ?> volta/e </p>
+
+
